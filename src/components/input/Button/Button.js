a7ed4a0349d35945a7d37d3185d844758e353b29
Andrew Rossi <aroccorossi@gmail.com>
2018-08-20 12:26:49 -0400

    configs are up to snuff

--- src/components/input/Button/Button.js
+++ src/components/input/Button/Button.js
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 export const Button = ({ onClick, children }) => (
   <button onClick={onClick}>{children}</button>
 );
+
+Button.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+};
