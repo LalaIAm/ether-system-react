d83da5eaaae65d43eb691ac48a36d3c88b7ec34c
Andrew Rossi <aroccorossi@gmail.com>
2018-08-21 10:16:36 -0400

    more configuration

--- src/components/input/Button/Button.js
+++ src/components/input/Button/Button.js
@@ -5,7 +5,18 @@ export const Button = ({ onClick, children }) => (
   <button onClick={onClick}>{children}</button>
 );
 
+export const ButtonSizes = ['small', 'medium', 'large'];
+
 Button.propTypes = {
+  /** Content to be rendered inside a Button */
   children: PropTypes.node,
+  /** Function to run on click */
   onClick: PropTypes.func,
+  /** Size of the Button */
+  size: PropTypes.oneOf(ButtonSizes),
 };
+
+Button.defaultProps = {
+  onClick: () => {},
+  size: 'medium',
+}
