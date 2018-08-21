ea0b368ce0411eb7d0979fd9b6c19104001d3a92
Andrew Rossi <aroccorossi@gmail.com>
2018-08-21 10:22:04 -0400

    button example looks good for now

--- src/components/input/Button/Button.js
+++ src/components/input/Button/Button.js
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: salmon;
+  border: 1px solid indianred;
+  border-radius: 3px;
+  color: snow;
+  padding: 9px 12px;
+`;
 
 export const Button = ({ onClick, children }) => (
-  <button onClick={onClick}>{children}</button>
+  <StyledButton onClick={onClick}>{children}</StyledButton>
 );
 
 export const ButtonSizes = ['small', 'medium', 'large'];
+export const ButtonModes = ['primary', 'secondary', 'warning', 'success', 'info'];
 
 Button.propTypes = {
   /** Content to be rendered inside a Button */
   children: PropTypes.node,
+  /** Mode of the button */
+  mode: PropTypes.oneOf(ButtonModes),
   /** Function to run on click */
   onClick: PropTypes.func,
   /** Size of the Button */
