f8cb421deddbbd68a3fa895ce3ede87a05007772
Andrew Rossi <aroccorossi@gmail.com>
2018-08-21 11:06:53 -0400

    add layout colors

--- src/base/color/colors.js
+++ src/base/color/colors.js
@@ -1,3 +1,13 @@
-const colors = {};
+const colors = {
+  // Layout
+  dark: '#0F1D2B',
+  dark85: '#3F4F5F',
+  dark75: '#5B6B7C',
+  dark50: '#91A3B0',
+  light110: '#B6C7D4',
+  light: '#E2EAF0',
+  light90: '#EFF4F8',
+  white: '#FFFFFF',
+};
 
 export default colors;
