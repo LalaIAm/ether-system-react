c159243b48d3ab76bdf41ab2acf72819b840fec2
Andrew Rossi <aroccorossi@gmail.com>
2018-08-27 12:27:32 -0400

    adding test variables

--- src/base/color/colorVariables.js
+++ src/base/color/colorVariables.js
@@ -1,3 +1,8 @@
-const colorVariables = {};
+import colors from './colors';
+
+const colorVariables = {
+  buttonPrimaryBackground: colors.blue,
+  buttonPrimaryBackgroundHover: colors.blueDark,
+};
 
 export default colorVariables;
