8492d47115a0a499c2c2456b83c4bb44c6366b7e
Andrew Rossi <aroccorossi@gmail.com>
2018-08-27 10:24:52 -0400

    add actual color swatches with styled titles

--- src/styleguide/LayoutColorPalette.jsx
+++ src/styleguide/LayoutColorPalette.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import colors from '../base/color/colors';
 import { ColorSwatch } from './components/ColorSwatch';
 
+const SwatchContainer = styled.div`
+  align-content: stretch;
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+`;
+
 export const LayoutColorPalette = () => {
-  // console.log(colors);
-
   const renderColorSwatches = Object.entries(colors).map((color) => {
-    console.log(color);
+    const [name, value] = color;
 
+    return (
+      <ColorSwatch name={name} value={value} />
+    );
   });
 
   return (
-    <ColorSwatch />
+    <SwatchContainer>
+      {renderColorSwatches}
+    </SwatchContainer>
   );
 };
--- src/styleguide/components/ColorSwatch.jsx
+++ src/styleguide/components/ColorSwatch.jsx
@@ -3,32 +3,49 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSwatch = styled.div`
+  box-sizing: border-box;
   padding: 10px;
+  width: 50%;
+`;
+
+const StyledSwatchTitle = styled.span`
+  background-color: #FFF;
+  color: #333;
+  display: inline-block;
+  font-size: 11px;
+  font-weight: 700;
+  margin-bottom: 5px;
+  padding: 8px;
+`;
+
+const StyledSwatchSubtitle = styled(StyledSwatchTitle)`
+  font-size: 10px;
 `;
 
 export const ColorSwatch = ({ height, name, value, width }) => {
   const swatchStyles = {
     backgroundColor: value,
-    height: `${height}px`,
-    width: `${width}px`,
+    height,
+    width,
   };
 
   return (
     <StyledSwatch style={swatchStyles}>
-      <p>{name}</p>
-      <p>{value}</p>
+      <StyledSwatchTitle>{name}</StyledSwatchTitle>
+      <br />
+      <StyledSwatchSubtitle>{value}</StyledSwatchSubtitle>
     </StyledSwatch>
   );
 };
 
 ColorSwatch.propTypes = {
-  height: PropTypes.number,
+  height: PropTypes.string,
   name: PropTypes.string,
   value: PropTypes.string,
-  width: PropTypes.number,
+  width: PropTypes.string,
 };
 
 ColorSwatch.defaultProps = {
-  height: 180,
-  width: 320,
+  height: '180px',
+  width: '50%',
 };
