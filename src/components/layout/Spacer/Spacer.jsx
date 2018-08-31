2a5e94f1b4e7e771d4442588748cf04f3eb0518e
Andy Rossi <andrewrocco@users.noreply.github.com>
2018-08-31 15:53:17 -0400

    Responsive Spacer (#12)
    * starting responsive work
    
    * refactor of spaceSize
    
    * adding responsive size generator
    
    * adding documentation
    
    * updated specs to include responsiveSizes tests
    
    * rename to spacerSizes

--- src/components/layout/Spacer/Spacer.jsx
+++ src/components/layout/Spacer/Spacer.jsx
@@ -1,19 +1,37 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import spacerSizes from '../../../base/spacing';
+import mediaQueries from '../../../base/breakpoints';
 
-export const Spacer = ({ debug, horizontal, size, ...others }) => {
+export const Spacer = ({ debug, horizontal, responsiveSizes, size, ...others }) => {
   const spacerBackground = debug ? 'pink' : 'transparent';
   const spacerPadding = horizontal ? 'padding-right' : 'padding-bottom';
   const spacerDisplay = horizontal ? 'inline-block' : 'block';
   const spacerHeight = (horizontal && debug) ? '5px' : '0';
+
+  // mutate the padding based on mediaQuery settings
+  const paddingSize = spacerSizes[size] || '0px';
+  let responsivePaddingSizes = css``;
+
+  if (responsiveSizes) {
+    responsivePaddingSizes = Object.entries(responsiveSizes).map((respSize) => {
+      const [bp, sp] = respSize;
+      return css`
+        ${mediaQueries[bp]} {
+          ${spacerPadding}: ${spacerSizes[sp]};
+        }
+      `;
+    });
+  }
+
   const StyledSpacer = styled.div`
     background-color: ${spacerBackground};
     display: ${spacerDisplay};
     height: ${spacerHeight};
-    ${spacerPadding}: ${spacerSizes[size] || '0px'}
+    ${spacerPadding}: ${paddingSize};
+    ${responsivePaddingSizes};
     vertical-align: middle;
   `;
 
@@ -29,6 +47,15 @@ Spacer.propTypes = {
   debug: PropTypes.bool,
   /** Sets to horizontal spacing mode */
   horizontal: PropTypes.bool,
+  /** Object containing breakpoint-spacer size key-value pairs. See below for example. */
+  responsiveSizes: PropTypes.shape({
+    xs: PropTypes.oneOf(SpacerSizes),
+    sm: PropTypes.oneOf(SpacerSizes),
+    md: PropTypes.oneOf(SpacerSizes),
+    lg: PropTypes.oneOf(SpacerSizes),
+    xl: PropTypes.oneOf(SpacerSizes),
+    xxl: PropTypes.oneOf(SpacerSizes),
+  }),
   /** Size of the spacer. Options are 'none', '2xsmall', 'xsmall', 'small', 'medium', 'large', 'xlarge', and '2xlarge' */
   size: PropTypes.oneOf(SpacerSizes),
 };
--- src/components/layout/Spacer/Spacer.md
+++ src/components/layout/Spacer/Spacer.md
@@ -12,3 +12,18 @@ const { Button } = require('../../input/Button/Button.jsx');
   <Button>Button 2</Button>
 </span>
 ```
+
+### Responsive Spacer
+It's possible to add a `responsiveSizes` configuration object to change the size of the Spacer at different viewport sizes. Setting this value will override the `size` prop value.
+```jsx static
+const responsiveSizes = {
+  xs: '2xsmall',
+  sm: 'xlarge',
+  lg: '2xlarge',
+};
+
+return (<Spacer responsiveSizes={responsiveSizes} />);
+```
+```js
+<Spacer debug responsiveSizes={{ xs: '2xsmall', sm: 'xlarge', lg: '2xlarge' }} />
+```
