de5cb886caaeeb27a66db80f583e0c5d511096b1
Andy Rossi <andrewrocco@users.noreply.github.com>
2018-09-04 13:45:12 -0400

    Build Scripts (#16)
    * add entry point
    
    * export everything available
    
    * set mode to production for now
    
    * remove from config, put in scripts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 export * from './components';
+
+export { default as mediaQueries } from './base/breakpoints';
+export { default as colors } from './base/color/colors';
+export { default as colorVariables } from './base/color/colorVariables';
+export { default as fontSizes } from './base/typography/fontSizes';
+export { default as spacerSizes } from './base/spacing';
