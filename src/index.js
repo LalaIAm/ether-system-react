b3ffd5ef51eb81b77e88c33d6dabf43a11684a5b
Andy Rossi <andrewrocco@users.noreply.github.com>
2018-09-04 16:06:38 -0400

    add a better README file (#17)
    * add a better README file
    
    * fix typo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { default as mediaQueries } from './base/breakpoints';
 export { default as colors } from './base/color/colors';
 export { default as colorVariables } from './base/color/colorVariables';
 export { default as fontSizes } from './base/typography/fontSizes';
+export { default as typography } from './base/typography/typography';
 export { default as spacerSizes } from './base/spacing';
